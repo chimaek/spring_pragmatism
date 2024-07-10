1bf26046a7d98e8b2a96cc7a3c1c6a8554b69fe2
seungju.kim <pipiru100@gmail.com>
2024-07-11 03:05:38 +0900

    init

--- src/main/java/net/chimaek/pragmatism_spring/ch01/RestaurantChain.java
+++ src/main/java/net/chimaek/pragmatism_spring/ch01/RestaurantChain.java
@@ -3,5 +3,33 @@ package net.chimaek.pragmatism_spring.ch01;
 import java.util.List;
 
 public class RestaurantChain {
-    private List<Store> stores;
+  private List<Store> stores;
+
+  public long calculateRevenue() {
+    long revenue = 0;
+    for (Store store : stores) {
+      for (Order order : store.getOrders()) {
+        for (Food food : order.getFoods()) {
+          revenue += food.getPrice();
+        }
+      }
+    }
+    return revenue;
+  }
+
+  public long calcualteProfit() {
+    long cost = 0;
+    for (Store store : stores) {
+      for (Order order : store.getOrders()) {
+        long orderPrice = 0;
+        for (Food food : order.getFoods()) {
+          orderPrice += food.getPrice();
+          cost += food.getOriginPrice();
+        }
+        cost += orderPrice * order.getTransctionFeePercent();
+      }
+      cost += store.getRentalFree();
+    }
+    return calculateRevenue() - cost;
+  }
 }
